206c5bd36532147d946d5caa91502c5701f424ab
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-12 11:59:38 -0600

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -25,8 +25,8 @@ describe("Quotes app", () => {
     textInput().should("exist");
     cy.get('input[name="xxxxx"]').should("not.exist");
     authorInput().should("exist");
-    cy.get("#submitBtn").should("exist");
-    cy.get("#cancelBtn").should("exist");
+    submitButton().should("exist");
+    cancelButton().should("exist");
 
     cy.contains("Submit Quote");
     cy.contains(/submit quote/i);
@@ -37,12 +37,12 @@ describe("Quotes app", () => {
     // assert they are empty
     // type in them
     // assert that the thing we typed is there
-    cy.get('input[name="text"]')
+    textInput()
       .should("have.value", "")
       .type("have fun learning React")
       .should("have.value", "have fun learning React");
 
-    cy.get('input[name="author"]')
+    authorInput()
       .should("have.value", "")
       .type("J. R. R. Tolkien")
       .should("have.value", "J. R. R. Tolkien");
