311fe176470728b4ad9d195f6cc2645f60c657ae
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-12 12:18:15 -0600

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -55,5 +55,11 @@ describe("Quotes app", () => {
     // syntax you will need:
     // "be.disabled"
     // .clear()
+    //The value of the inputs should be ""
+    //The submit button should be disabled
+    //Input "Philosophy is the cave man way" into text input
+    //Assert submit button should be disabled
+    //Input "Grok" into author input
+    //Assert Submit is not disabled
   });
 });
